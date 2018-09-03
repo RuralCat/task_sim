8ef8ceecacee8b040b5fe19c4146091014049c5a
Marine <wangmy11@163.com>
2018-09-03 19:53:30 +0800

    imaging

--- example.py
+++ example.py
@@ -2,7 +2,11 @@
 
 from simpy import Environment
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+
 
 def clock(env, name, tick, num):
     assert isinstance(env, Environment)
@@ -37,31 +41,39 @@ class Car(object):
         yield self.env.timeout(duration)
 
 if __name__ == '__main__':
-    param_l = np.arange(55,80,1)
-    mean_time_l =  [8.93, 8.929, 8.641, 8.878, 8.778, 8.848, 8.78, 8.574, 8.736, 8.538, 8.63, 8.597, 8.745, 8.491, 8.373, 8.59, 8.67,
-     8.672, 8.462, 8.508, 8.601, 8.475, 8.481, 8.403, 8.352]
-    lvyue_rate_l = [0.992, 0.992, 0.995, 0.993, 0.991, 0.992, 0.994, 0.992, 0.993, 0.994, 0.994, 0.993, 0.993, 0.995, 0.996, 0.996,
-     0.994, 0.993, 0.996, 0.994, 0.992, 0.993, 0.995, 0.995, 0.995]
-    mean_os_work_time_l =  [18.1, 18.1, 18.0, 18.0, 18.0, 18.0, 18.0, 18.0, 18.0, 18.0, 18.0, 18.0, 18.0, 18.0, 18.0, 18.0, 18.0, 18.0, 18.0,
-     18.0, 18.0, 18.0, 18.0, 18.0, 18.0]
-    mean_ss_work_time_l = [20.0, 20.1, 20.0, 20.2, 20.0, 20.3, 20.3, 20.0, 20.1, 20.0, 20.1, 20.0, 20.2, 20.1, 19.9, 20.2, 20.4, 20.3, 20.1,
-     20.4, 20.2, 20.1, 20.2, 20.1, 20.0]
+    param_l = np.arange(15, 35, 1)
+    mean_time_l = [15.58, 15.181, 14.88, 14.622, 14.137, 13.612, 13.268, 12.966, 12.629, 12.315, 12.058, 11.78, 11.472, 11.394, \
+                   11.338, 11.209, 11.225, 11.256, 11.223, 11.224]
+    completion_in24_rate_l = [0.823, 0.848, 0.873, 0.895, 0.946, 0.974, 0.988, 0.997, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+    mean_os_work_time_l = [18.2, 18.2, 18.2, 18.3, 18.2, 18.2, 18.2, 18.2, 18.2, 18.2, 18.2, 18.2, 18.2, 18.2, 18.2, 18.2, 18.2, 18.2, 18.2, 18.2]
+    mean_ss_work_time_l = [24.0, 23.9, 23.7, 22.9, 22.2, 21.4, 20.8, 20.3, 19.9, 19.4, 19.0, 18.6, 18.3, 18.2, 18.1, 18.0, 18.0, 18.0, 18.0, 18.0]
 
+    plt.rcParams['font.sans-serif'] = ['Microsoft Yahei']
 
-    result = pd.merge(pd.merge(pd.merge(mean_time_l, lvyue_rate_l), pd.merge(mean_os_work_time_l, mean_ss_work_time_l)),param_l)
-    title = "客服数量与理赔综合指标"
+    title = "自营人数变化影响 - 外包88人"
+    xmajorLocator = MultipleLocator(1)  # 将x主刻度标签设置为1的倍数
+    xmajorFormatter = FormatStrFormatter('%1.1f')  # 设置x轴标签文本的格式
+
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
-    ax1.plot(param_l, mean_time_l)
-    ax1.set_ylabel('mean_time_l')
-    ax1.set_title("mean_time_l")
+    ax1.plot(param_l, mean_time_l,label = "平均时效")
+    ax1.plot(param_l, mean_ss_work_time_l,label = '自营平均下班时间')
+    ax1.xaxis.set_major_locator(xmajorLocator)
+    ax1.set_xlabel('自营人数',fontsize=14)
+    ax1.set_ylabel('时效/下班时间',fontsize=14)
+
+    ax1.set_xlim([10, 16])
+    ax1.set_ylim([5, 26])
+    ax1.legend(loc=4)
 
     ax2 = ax1.twinx()
-    ax2.plot(param_l, lvyue_rate_l, 'r')
-    ax2.set_ylabel('lvyue_rate_l')
-    ax2.set_xlabel('lvyue_rate_l')
+    ax2.plot(param_l, completion_in24_rate_l, ':',label = '24小时履约率')
+    ax2.set_ylabel('24小时履约率',fontsize=14)
+    ax2.set_ylim([0.5, 1.1])
+    ax2.legend(loc=0)
 
     ax1.grid(True)
-    plt.title(title)
+    #ax2.legend(loc=0)
+    plt.title(title,fontsize=18) #fontproperties="Microsoft Yahei"
     plt.show()
 
